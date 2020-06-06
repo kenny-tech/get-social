7cd55389cddd5a4536af78811a9d50bbd03cdda0
kenny-tech <kennyemma2008@gmail.com>
2020-06-06 20:44:16 +0100

    Created signin screen and updated signup screen

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignupScreen from './app/screens/Signup';
+import SigninScreen from './app/screens/Signin'
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName="SigninScreen" mode="modal">
       <Stack.Navigator>
         <Stack.Screen options={{headerShown: false}} name="Signup" component={SignupScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Signin" component={SigninScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
