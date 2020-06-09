139ae428374b57a83119b99b0bc4f43a51022844
kenny-tech <kennyemma2008@gmail.com>
2020-06-09 21:42:48 +0100

    Created contact screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SignupScreen from './app/screens/Signup';
 import SigninScreen from './app/screens/Signin';
 import WelcomeScreen from './app/screens/Welcome';
+import ContactScreen from './app/screens/Contact';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <NavigationContainer mode="modal">
       <Stack.Navigator>
+        <Stack.Screen name="Contact" component={ContactScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen options={{headerShown: false}} name="Signin" component={SigninScreen} />
         <Stack.Screen options={{headerShown: false}} name="Signup" component={SignupScreen} />
