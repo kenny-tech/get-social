d36fc1a10418dc3a15458eee451be81d6255bf7e
kenny-tech <kennyemma2008@gmail.com>
2020-06-11 08:38:09 +0100

    Updated post screen with static images

--- App.js
+++ App.js
@@ -7,13 +7,14 @@ import SignupScreen from './app/screens/Signup';
 import SigninScreen from './app/screens/Signin';
 import ProfileScreen from './app/screens/Profile';
 import ContactScreen from './app/screens/Contact';
+import PostScreen from './app/screens/Post';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer mode="modal">
-      <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}} initialRouteName="Profile">
+      <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}} initialRouteName="Post">
       <Stack.Screen name="Profile" component={ProfileScreen} options={{
           headerRight: () => (
             <TouchableOpacity onPress={() => {Alert.alert("Get Social would like to access your contacts", "We want to connect you with your friends")}}>
@@ -24,6 +25,7 @@ function App() {
         <Stack.Screen options={{headerShown: false}} name="Signup" component={SignupScreen} />
         <Stack.Screen options={{headerShown: false}} name="Signin" component={SigninScreen} />
         <Stack.Screen name="Contact" component={ContactScreen} />
+        <Stack.Screen name="Post" component={PostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/screens/Post.js
+++ app/screens/Post.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+
+import styles from '../styles/style';
+import image1 from '../images/phone.jpg';
+import image2 from '../images/macbookpro.jpg';
+import image3 from '../images/ecommerce.jpg';
 
 const Post = () => {
+
+    const [feed, setFeed] = useState([
+        {
+            user: 'Kenny',
+            caption: 'My first photo',
+            url: image1,
+        },
+        {
+            user: 'John',
+            caption: 'My second photo',
+            url: image2,
+        },
+        {
+            user: 'Peter',
+            caption: 'My third photo',
+            url: image3
+        }
+    ])
+
     return (
-        <View>
-            <Text>Post</Text>
+        <View style={styles.container}>
+            <FlatList
+                data={feed}
+                renderItem={({ item }) => (
+                    <View>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20, marginVertical: 5}}>
+                            <Text>@{item.user}</Text>
+                        </View>
+                        <Image source={item.url} style={{width: 370, height: 200, marginVertical: 10, marginHorizontal: 20}}/>
+                        <Text style={{marginHorizontal: 20}}>{item.caption}</Text>
+                    </View>
+                )}
+                keyExtractor={item => item.id}
+            />
         </View>
     )
 }
